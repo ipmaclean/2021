aae01edb7aaae759af261b036fc5996c9de2d62f
Iain Maclean <iain.p.m.maclean@gmail.com>
2021-12-25 10:47:45 +0000

    Solved Day 25

---
+++ Day 25/Day 25/Cucumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_25
+{
+    public class Cucumber
+    {
+        public char Facing { get; set; }
+        public (int, int) Coords { get; set; }
+        public bool WillMove { get; set; } = false;
+
+        public Cucumber(char facing, (int, int) coords)
+        {
+            Facing = facing;
+            Coords = coords;
+        }
+
+        public void Move(int columnCount, int rowCount)
+        {
+            if (Facing == '>')
+            {
+                if (Coords.Item1 < columnCount - 1)
+                {
+                    Coords = (Coords.Item1 + 1, Coords.Item2);
+                }
+                else
+                {
+                    Coords = (0, Coords.Item2);
+                }
+            }
+            else
+            {
+                if (Coords.Item2 < rowCount - 1)
+                {
+                    Coords = (Coords.Item1, Coords.Item2 + 1);
+                }
+                else
+                {
+                    Coords = (Coords.Item1, 0);
+                }
+            }
+            WillMove = false;
+        }
+    }
+}
---
+++ Day 25/Day 25/InputHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_25
+{
+    public class InputHelper
+    {
+        public static List<Cucumber> Parse(string fileName)
+        {
+            var input = Path.Combine("..", "..", "..", fileName);
+            var cucumbers = new List<Cucumber>();
+
+            using (var sr = new StreamReader(input))
+            {
+                string ln;
+                var rowIndex = 0;
+                while ((ln = sr.ReadLine()) != null)
+                {
+                    for (int i = 0; i < ln.Length; i ++)
+                    {
+                        if (ln[i] == '.') continue;
+                        var cucumber = new Cucumber(ln[i], (i, rowIndex));
+                        cucumbers.Add(cucumber);
+                    }
+                    rowIndex++;
+                }
+            }
+            return cucumbers;
+        }
+
+        public static int CountRows(string fileName)
+        {
+            var input = Path.Combine("..", "..", "..", fileName);
+            var file = new FileInfo(input);
+            return File.ReadLines(file.FullName).Count();
+        }
+
+        public static int CountColumns(string fileName)
+        {
+            var input = Path.Combine("..", "..", "..", fileName);
+            using (var sr = new StreamReader(input))
+            {
+                return sr.ReadLine().Length;
+            }
+        }
+    }
+}
